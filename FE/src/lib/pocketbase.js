0813cf73873c13389f6eb27d70bbc87819e33ace
unknown <camlyphamthi31@gmail.com>
2026-04-17 14:46:08 +0700

    Đổi link pocketbase

--- FE/src/lib/pocketbase.js
+++ FE/src/lib/pocketbase.js
@@ -2,6 +2,6 @@ import PocketBase from 'pocketbase';
 
 // Khởi tạo kết nối tới server PocketBase của bạn
 // (Mặc định PocketBase chạy ở cổng 8090 trên máy tính)
-const pb = new PocketBase('https://husc-api.pockethost.io'); 
+const pb = new PocketBase('http://127.0.0.1:8090');
 
 export default pb;
